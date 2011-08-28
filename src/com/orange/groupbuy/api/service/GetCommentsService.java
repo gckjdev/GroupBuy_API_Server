81a8ebfab2b712a12c37ae2c97fee5915fb90a09
Lizhang Peng <penglzh2010@qq.com>
2011-08-28 18:51:11 +0800

    update

--- src/com/orange/groupbuy/api/service/GetCommentsService.java
+++ src/com/orange/groupbuy/api/service/GetCommentsService.java
@@ -1,8 +1,21 @@
 package com.orange.groupbuy.api.service;
 
+import java.util.Date;
+import java.util.Iterator;
+
 import javax.servlet.http.HttpServletRequest;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.orange.common.utils.DateUtil;
+import com.orange.groupbuy.constant.DBConstants;
+import com.orange.groupbuy.constant.ErrorCode;
 import com.orange.groupbuy.constant.ServiceConstant;
+import com.orange.groupbuy.dao.Product;
+import com.orange.groupbuy.manager.ProductManager;
 
 public class GetCommentsService extends CommonGroupBuyService {
 	String appId;
@@ -16,7 +29,26 @@ public class GetCommentsService extends CommonGroupBuyService {
 	
 	@Override
 	public void handleData() {	
+		Product product = ProductManager.findProductById(mongoClient, productId);
+		if (product == null){
+			resultCode = ErrorCode.ERROR_PRODUCT_NOT_FOUND;
+			return;
+		}
 		
+		JSONArray commentsArray = new JSONArray();
+		BasicDBList comments = product.getComments();
+		Iterator<Object> iterator = comments.iterator();
+		while (iterator.hasNext()) {
+			BasicDBObject comment = (BasicDBObject)iterator.next();
+			JSONObject commentObject = new JSONObject();
+			commentObject.put(ServiceConstant.PARA_NICKNAME, comment.getString(DBConstants.F_NICKNAME));
+			commentObject.put(ServiceConstant.PARA_COMMENT_CONTENT, comment.getString(DBConstants.F_COMMENT_CONTENT));
+			Date date = (Date)comment.get(DBConstants.F_CREATE_DATE);
+			commentObject.put(ServiceConstant.PARA_CREATE_DATE, DateUtil.dateToString(date));
+			commentsArray.add(commentObject);
+		}
+		
+		resultData = commentsArray;
 	}
 	
 	@Override
