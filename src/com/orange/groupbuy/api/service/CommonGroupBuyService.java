adabf972d15fceb563f5e7ec849c120a221c771f
Benson Peng <penglingzhe@gmail.com>
2011-09-01 14:15:31 +0800

    add log4j support

--- src/com/orange/groupbuy/api/service/CommonGroupBuyService.java
+++ src/com/orange/groupbuy/api/service/CommonGroupBuyService.java
@@ -1,13 +1,6 @@
 package com.orange.groupbuy.api.service;
 
-
-
-import java.util.HashMap;
-import java.util.Map;
-
 import com.orange.common.api.service.CommonService;
-import com.orange.groupbuy.api.service.RegisterDeviceService;
-import com.orange.groupbuy.constant.ServiceConstant;
 
 public abstract class CommonGroupBuyService extends CommonService {
 
